a830ebaed71be6b314db415a22f726f713d4f3ba
kauessilvas <kauesantosds5@gmail.com>
2025-05-10 14:49:12 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int number;
+
+Console.WriteLine("====== Impar ou Par ======");
+while (true)
+{
+    Console.Write("Digite um número: ");
+    string input = Console.ReadLine()!;
+
+    if (int.TryParse(input, out number))
+    {
+        break; 
+    }
+    else
+    {
+        Console.WriteLine("Entrada inválida. Tente novamente.");
+    }
+}
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("O número é par.");
+}
+else
+{
+    Console.WriteLine("O número é ímpar.");
+}
